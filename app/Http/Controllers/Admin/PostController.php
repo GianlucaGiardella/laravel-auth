751d3f0faae2003f4913a34c4682c15388b4e3f0
GianluGiarde <gianlugiarde@gmail.com>
2023-07-06 18:59:46 +0200

    add posts controller

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::paginate(10);
+
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Post $post)
+    {
+        return view('admin.posts.show', compact('post'));
+    }
+
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
