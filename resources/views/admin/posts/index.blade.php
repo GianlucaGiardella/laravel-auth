8e3422e9e0887595ea635e121d462bb3a366893d
GianluGiarde <gianlugiarde@gmail.com>
2023-07-07 12:55:19 +0200

    add delete modal & alert

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,6 +3,13 @@
 @section('contents')
     <h1>Posts</h1>
 
+    @if (session('delete_success'))
+        @php $post = session('delete_success') @endphp
+        <div class="alert alert-danger">
+            Il Post "{{ $post->title }}" è stato eliminato
+        </div>
+    @endif
+
     <table class="table table-striped">
         <thead>
             <tr>
@@ -21,15 +28,31 @@
                     <td>
                         <a class="btn btn-primary" href="{{ route('admin.posts.show', ['post' => $post->id]) }}">View</a>
                         <a class="btn btn-warning" href="{{ route('admin.posts.edit', ['post' => $post->id]) }}">Edit</a>
-                        <form action="{{ route('admin.posts.destroy', ['post' => $post->id]) }}" method="post"
-                            class="d-inline-block">
-                            @csrf
-                            @method('delete')
-                            <button class="btn btn-danger">Delete</button>
-                        </form>
+                        <button class="btn btn-danger js-delete" data-bs-toggle="modal" data-bs-target="#deleteModal"
+                            data-id="{{ $post->id }}">Delete</button>
                     </td>
                 </tr>
             @endforeach
+
+            <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="deleteModalLabel">Are you sure ?</h1>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <form action="" data-template="{{ route('admin.posts.destroy', ['post' => '*****']) }}"
+                                method="post" class="d-inline-block" id="confirm-delete">
+                                @csrf
+                                @method('delete')
+                                <button class="btn btn-danger">Delete</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </tbody>
     </table>
 
